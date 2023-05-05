9035e474730a5dc8b13db733c0aaca66ecf31de5
Vladislav <lycakvladislav@gmail.com>
2023-05-05 18:50:20 -0400

    add main menu component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import CallBackForm from './components/CallBackForm/CallBackForm';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import TopMenu from './components/TopMenu/TopMenu';
 
 function App() {
   return (
     <div className="body">
       <Header />
       <div className="main">
+        <TopMenu />
         <CallBackForm />
       </div>
       <Footer />
---
+++ src/Components/TopMenu/TopMenu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TopMenu.scss';
+
+const TopMenu = () => {
+  return (
+    <div className="top-menu">
+      <div className="container">
+        <div className="topm-menu__wrapper">
+          <div>MainMenu</div>
+          <div>TrackDelivery</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopMenu;
