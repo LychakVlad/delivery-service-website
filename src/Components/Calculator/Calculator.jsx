d075f626d2de50a34d180294d1f644fdec8052c6
Vladislav <lycakvladislav@gmail.com>
2023-05-07 22:42:41 -0400

    add total calculator section

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -3,6 +3,7 @@ import './Calculator.scss';
 import SecondTitle from '../ui/titles/SecondTitle';
 import ThirdTitle from '../ui/titles/ThirdTitle';
 import Box from '../../assets/box.svg';
+import Icon from '../../assets/calc-icon.svg';
 import Input from '../form/Input/Input';
 import Button from '../ui/Button';
 import Toggle from '../form/toggle/Toggle';
@@ -137,6 +138,53 @@ const Calculator = () => {
                   onClick={() => handleItemClick(item.id)} // pass a callback to handle item clicks
                 />
               ))}
+            </div>
+            <div className="calculator__total">
+              <div className="calculator__time">
+                <div className="calculator__time-option">
+                  <img src={Icon} alt="icon" />
+                  <div className="calculator__time-right">
+                    <p className="calculator__time-text">Post Office</p>
+                    <p className="calculator__time-days">7-14 days</p>
+                  </div>
+                </div>
+                <div className="calculator__time-option">
+                  <img src={Icon} alt="icon" />
+                  <div className="calculator__time-right">
+                    <p className="calculator__time-text">Post Office</p>
+                    <p className="calculator__time-days">7-14 days</p>
+                  </div>
+                </div>
+                <div className="calculator__time-option">
+                  <img src={Icon} alt="icon" />
+                  <div className="calculator__time-right">
+                    <p className="calculator__time-text">Post Office</p>
+                    <p className="calculator__time-days">7-14 days</p>
+                  </div>
+                </div>
+              </div>
+              <div className="calculator__total-price-block">
+                <p className="calculator__total-price">20$</p>
+                <p className="calculator__total-price">40$</p>
+                <p className="calculator__total-price">80$</p>
+              </div>
+              <div className="calculator__total-plan">
+                <ThirdTitle title="What is included in the rate:" />
+                <ul className="calculator__total-plan-list">
+                  <li className="calculator__total-plan-point">
+                    Notifying recipients
+                  </li>
+                  <li className="calculator__total-plan-point">
+                    Receiving orders at sorting centers and delivery points
+                  </li>
+                  <li className="calculator__total-plan-point">
+                    Checking completeness
+                  </li>
+                  <li className="calculator__total-plan-point">
+                    Weekend delivery
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
