d5e90d12abbe031b2735feab0f8f5b261aa3e76a
Vladislav <lycakvladislav@gmail.com>
2023-05-18 18:32:55 -0400

    fix dropdown menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { ReactComponent as CallIcon } from '../../assets/call-icon.svg';
+import { ReactComponent as Arrow } from '../../assets/arrow.svg';
 import './Header.scss';
 import { useLocation } from 'react-router-dom';
 import Dropdown from '../ui/dropdown/Dropdown';
@@ -7,6 +8,8 @@ import Dropdown from '../ui/dropdown/Dropdown';
 const Header = () => {
   const [rounded, setRounded] = useState('');
   const [dropdown, setDropdown] = useState(false);
+  let ref = useRef();
+
   let location = useLocation();
 
   useEffect(() => {
@@ -15,7 +18,29 @@ const Header = () => {
     }
   }, [location.pathname]);
 
-  const links = ['Main', 'XDoc', 'Widget', 'About', 'Contact'];
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdown && ref.current && !ref.current.contains(event.target)) {
+        setDropdown(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    document.addEventListener('touchstart', handler);
+    return () => {
+      document.removeEventListener('mousedown', handler);
+      document.removeEventListener('touchstart', handler);
+    };
+  }, [dropdown]);
+
+  const onMouseEnter = () => {
+    window.innerWidth > 960 && setDropdown(true);
+  };
+
+  const onMouseLeave = () => {
+    window.innerWidth > 960 && setDropdown(false);
+  };
+
+  const links = ['Main', 'XDoc', 'About', 'Contact', 'Widgets'];
 
   const dropdownLinks = [
     {
@@ -23,6 +48,7 @@ const Header = () => {
       link: '/cms',
     },
     { title: 'API', link: '/api' },
+    { title: 'Widget', link: '/widget' },
   ];
 
   return (
@@ -49,10 +75,9 @@ const Header = () => {
           <nav className="header-bottom-menu">
             <ul className="header__nav-menu">
               {links.map((item, index) =>
-                item !== 'Widget' ? (
-                  <li className="header__nav-point">
+                item !== 'Widgets' ? (
+                  <li className="header__nav-point" key={index}>
                     <a
-                      key={index}
                       href={`/${item.toLocaleLowerCase()}`}
                       className={`header__bottom-menu-item ${
                         location.pathname === `/${item.toLocaleLowerCase()}`
@@ -64,19 +89,30 @@ const Header = () => {
                     </a>
                   </li>
                 ) : (
-                  <li className="header__nav-point">
-                    <a
-                      key={index}
+                  <li
+                    key={index}
+                    className="header__nav-point"
+                    ref={ref}
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                  >
+                    <p
                       onClick={() => setDropdown((prev) => !prev)}
-                      href={`/${item.toLocaleLowerCase()}`}
                       className={`header__bottom-menu-item ${
-                        location.pathname === `/${item.toLocaleLowerCase()}`
+                        location.pathname === '/cms' ||
+                        location.pathname === '/widget' ||
+                        location.pathname === '/api'
                           ? 'is-active'
                           : ''
                       }`}
                     >
                       {item}
-                    </a>
+                      <span className="arrow-container">
+                        <i className={`arrow ${dropdown ? 'rotate' : ''}`}>
+                          <Arrow />
+                        </i>
+                      </span>
+                    </p>
                     <Dropdown points={dropdownLinks} dropdown={dropdown} />
                   </li>
                 )
